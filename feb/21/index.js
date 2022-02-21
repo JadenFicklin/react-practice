562fd54dce9a8b7bb74610254cffd87c862e561a
JadenFicklin <jadendficklin@gmail.com>
2022-02-21 08:42:14 -0700

    codewars odd or even

---
+++ feb/21/index.js
@@ -0,0 +1,11 @@
+//codewars day 1
+//return if the parameter is odd or even
+
+const even_or_odd = (param) => {
+  if (param % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+};
+console.log(even_or_odd(1));
