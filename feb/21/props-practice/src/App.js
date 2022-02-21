d1c7b86ab2b28427dfebe93e9967622df89f81ef
JadenFicklin <jadendficklin@gmail.com>
2022-02-21 15:38:42 -0700

    props practice

---
+++ feb/21/props-practice/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Greet from "./Greet";
+
+function App() {
+  return (
+    <div>
+      <Greet name="Darth maul" />
+      <Greet name="Quoi guan gin" />
+      <Greet name="Obi wan kenobi" />
+    </div>
+  );
+}
+
+export default App;
---
+++ feb/21/props-practice/src/Greet.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Greet(props) {
+  return <div>Hello {props.name}</div>;
+}
+
+export default Greet;
