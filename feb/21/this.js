88ec2181116842d8981d87e4a206fb697a7d6d03
JadenFicklin <jadendficklin@gmail.com>
2022-02-21 09:20:27 -0700

    understanding this keyword part 2

--- feb/21/this.js
+++ feb/21/this.js
@@ -9,9 +9,19 @@ let message = {
   arrowFunction: () => console.log("Hi " + this.name),
 };
 
-message.regularFunction();
-message.arrowFunction();
-console.log(this); // it should console log the window object, but its just printing an empty object lol
+// message.regularFunction();
+// message.arrowFunction();
+// console.log(this); // it should console log the window object, but its just printing an empty object lol
 
 //this refers to the owner of the function we are executing
 //arrow functions do not have their own this value
+
+//part 2
+let person1 = { name: "John", age: 22 };
+let person2 = { name: "Mary", age: 26 };
+
+let sayHi = function () {
+  console.log("Hi, " + this.name);
+};
+
+sayHi.call(person2);
